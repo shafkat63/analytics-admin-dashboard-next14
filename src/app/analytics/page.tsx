23a4f98f67f719f0a3f860ff078c490685301bc0
shafkat63 <msm.shafkat99@gmail.com>
2024-02-14 01:11:32 +0600

    retrieveDays

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,6 +1,6 @@
 import { analytics } from "@/utils/analytics";
 const Analytics = async () => {
-	const pageview = await analytics.retrive("pageview", "11/02/2024");
+	const pageview = await analytics.retrieveDays("pageview", 2);
 	return <div  className="text-white">{JSON.stringify(pageview)}</div>;
 };
 
--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,5 +1,6 @@
 import { redis } from "@/lib/redis";
 import { getDate } from "@/utils/index";
+import { parse } from "date-fns";
 type AnalyticsArgs = {
 	retention?: number;
 };
@@ -26,7 +27,29 @@ export class Analytics {
 		await redis.hincrby(key, JSON.stringify(event), 1);
 		if (!opts?.persists) await redis.expire(key, this.retention);
 	}
-	async retrive(namespace: string, date: string) {
+	async retrieveDays(namespace: string, nDays: number) {
+		type AnalyticsPromise = ReturnType<typeof analytics.retrieve>;
+		const promises: AnalyticsPromise[] = [];
+		for (let i = 0; i < nDays; i++) {
+			const formattedDate = getDate(i);
+			const promise = analytics.retrieve(namespace, formattedDate);
+			promises.push(promise);
+		}
+		const fetched = await Promise.all(promises);
+		const data = fetched.sort((a, b) => {
+			if (
+				parse(a.date, "dd/MM/yyyy", new Date()) >
+				parse(b.date, "dd/MM/yyyy", new Date())
+			) {
+				return 1;
+			} else {
+				return -1;
+			}
+		});
+		return data;
+	}
+
+	async retrieve(namespace: string, date: string) {
 		const res = await redis.hgetall<Record<string, string>>(
 			`analytics::${namespace}::${date}`
 		);
