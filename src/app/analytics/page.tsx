22eb22af5a82a4eed54b09c1e4ac3d59cf47029c
shafkat63 <msm.shafkat99@gmail.com>
2024-02-14 01:52:17 +0600

    tracking the average visit of last 7 days

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,11 +1,22 @@
 import AnalyticsDashboard from "@/components/AnalyticsDashboard";
 import { analytics } from "@/utils/analytics";
 const Analytics = async () => {
-	const pageview = await analytics.retrieveDays("pageview", 2);
+	const TRACKING_DAYS = 7;
+
+	const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS);
+	const totalPageViews = pageviews.reduce((acc, curr) => {
+		return (
+			acc +
+			curr.events.reduce((acc, curr) => {
+				return acc + Object.values(curr)[0]!;
+			}, 0)
+		);
+	}, 0);
+	const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1);
 	return (
 		<div className="min-h-screen w-full py-12 flex justify-center items-center">
 			<div className="relative w-full max-w-6xl mx-auto text-white">
-				<AnalyticsDashboard />
+				<AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} />
 			</div>
 		</div>
 	);
